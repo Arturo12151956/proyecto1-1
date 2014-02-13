c04941a85ae37d9e3c33da25915b3de9a7939db6
Arturo Torres <arturo_torres_r@hotmail.com>
2014-02-13 13:27:47 -0600

    proyecto1-1

---
+++ js/acciones.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$(document).ready(function() {
+    documment.addeventlisterer("deviceready", function(){
+		
+$('#B1').tap(function(){	
+	navigator.notification.beep(1);
+		
+		}) // cierra tap b1
+	}); // cierra deviceready
+}); //cierra ready
